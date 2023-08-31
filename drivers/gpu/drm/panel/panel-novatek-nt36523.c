ff2eb36fbe2246107b5f8389be7986597aa93616
Jianhua Lu <lujianhua000@gmail.com>
2023-08-31 21:18:48 +0800

    drm/panel: nt36523: enable prepare_prev_first
    Signed-off-by: map220v <map220v300@gmail.com>

--- drivers/gpu/drm/panel/panel-novatek-nt36523.c
+++ drivers/gpu/drm/panel/panel-novatek-nt36523.c
@@ -1443,6 +1443,7 @@ static int nt36523_probe(struct mipi_dsi_device *dsi)
 
 	pinfo->dsi[0] = dsi;
 	mipi_dsi_set_drvdata(dsi, pinfo);
+	pinfo->panel.prepare_prev_first = true;
 
 	ret = of_drm_get_panel_orientation(dev->of_node, &pinfo->orientation);
 	if (ret < 0) {
